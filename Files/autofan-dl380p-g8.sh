1256854d054e29b748eec78abca231f0b60e22e8
chinky <chinky@163.com>
2022-09-25 00:18:56 +0800

    fix login

--- Files/autofan-dl380p-g8.sh
+++ Files/autofan-dl380p-g8.sh
@@ -9,9 +9,11 @@
 # rm mycron
 # chmod +x /autofan.sh
 #
-PASSWORD="YOURILOPASSWORD"
-USERNAME="YOURILOUSER"
-ILOIP="YOURILOIP"
+PASSWORD="your password"
+USERNAME="your username"
+ILOIP="your ilo ip"
+SSH_OPTIONS="-oKexAlgorithms=+diffie-hellman-group1-sha1 -oHostkeyAlgorithms=+ssh-rsa"
+
 T1="$(sensors -Aj coretemp-isa-0000 | jq '.[][] | to_entries[] | select(.key | endswith("input")) | .value' | sort -rn | head -n1)"
 T2="$(sensors -Aj coretemp-isa-0001 | jq '.[][] | to_entries[] | select(.key | endswith("input")) | .value' | sort -rn | head -n1)"
 
@@ -21,34 +23,34 @@ echo "==============="
 
 if [[ $T1 > 67 ]]
    then
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 3 max 255'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 4 max 255'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 5 max 255'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 3 max 255'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 4 max 255'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 5 max 255'
 elif [[ $T1 > 58 ]]
     then
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 3 max 39'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 4 max 39'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 5 max 39'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 3 max 39'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 4 max 39'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 5 max 39'
 
 elif [[ $T1 > 54 ]]
     then
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 3 max 38'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 4 max 38'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 5 max 38'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 3 max 38'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 4 max 38'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 5 max 38'
 elif [[ $T1 > 52 ]]
     then
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 3 max 34'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 4 max 34'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 5 max 34'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 3 max 34'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 4 max 34'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 5 max 34'
 elif [[ $T1 > 50 ]]
     then
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 3 max 32'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 4 max 32'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 5 max 32'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 3 max 32'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 4 max 32'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 5 max 32'
 else
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 3 max 30'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 4 max 30'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 5 max 30'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 3 max 30'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 4 max 30'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 5 max 30'
 fi
 
 echo "==============="
@@ -57,32 +59,32 @@ echo "==============="
 
 if [[ $T2 > 67 ]]
    then
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 0 max 255'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 1 max 255'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 2 max 255'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 0 max 255'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 1 max 255'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 2 max 255'
 
 elif [[ $T2 > 58 ]]
     then
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 0 max 39'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 1 max 39'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 2 max 39'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 0 max 39'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 1 max 39'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 2 max 39'
 elif [[ $T2 > 54 ]]
     then
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 0 max 38'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 1 max 38'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 2 max 38'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 0 max 38'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 1 max 38'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 2 max 38'
 elif [[ $T2 > 52 ]]
     then
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 0 max 34'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 1 max 34'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 2 max 34'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 0 max 34'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 1 max 34'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 2 max 34'
 elif [[ $T2 > 50 ]]
     then
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 0 max 32'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 1 max 32'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 2 max 32'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 0 max 32'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 1 max 32'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 2 max 32'
 else
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 0 max 30'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 1 max 30'
-        sshpass -p $PASSWORD ssh $USERNAME@$ILOIP 'fan p 2 max 30'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 0 max 30'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 1 max 30'
+        sshpass -p $PASSWORD ssh $SSH_OPTIONS $USERNAME@$ILOIP 'fan p 2 max 30'
 fi
--- Install.sh
+++ Install.sh
@@ -12,11 +12,11 @@ if [[ ${ACCEPTED,,} =~ ^[y] ]]; then
     cd ~/autofan
     echo "Installing required packages..."
     apt install sshpass wget lm-sensors jq -y
-    echo -e "\e[92mDownloading ILO_250 for ROM upgrade\e[0m"
-    wget -q https://gh.tj20.top/https://github.com/dongly/HP-ILO-Fan-Control/tree/me/Files/ilo_250
+    # echo -e "\e[92mDownloading ILO_250 for ROM upgrade\e[0m"
+    # wget -q https://gh.tj20.top/https://github.com/dongly/HP-ILO-Fan-Control/tree/me/Files/ilo_250
     echo -e "\e[92m Creating autofan service\e[0m"
-    wget -q https://gh.tj20.top/https://raw.githubusercontent.com/dongly/HP-ILO-Fan-Control/me/Files/autofan.service
-    mv autofan.service /etc/systemd/system/
+    # wget -q https://gh.tj20.top/https://raw.githubusercontent.com/dongly/HP-ILO-Fan-Control/me/Files/autofan.service
+    mv Files/autofan.service /etc/systemd/system/
     echo -e "\e[92m autofan service created\e[0m"
     read -rep $'Which server are you running? (Enter 1-4)
     1. DL360p G8 (No ESXi)
@@ -47,15 +47,18 @@ if [[ ${ACCEPTED,,} =~ ^[y] ]]; then
     esac
 
     echo "Preping autofan.sh for $HOSTTYPE"
-    echo "Downloading latest autofan.sh"
-    wget -q https://gh.tj20.top/https://raw.githubusercontent.com/dongly/HP-ILO-Fan-Control/me/Files/$AUTOFANFILE -O autofan.sh
+    echo "Installing autofan.sh"
+    # wget -q https://gh.tj20.top/https://raw.githubusercontent.com/dongly/HP-ILO-Fan-Control/me/Files/$AUTOFANFILE -O autofan.sh
+    cp ./Files/$AUTOFANFILE autofan
+    chmod +x autofan
     read -rep 'Enter iLO Username: ' ILOUSERNAME
     read -rep 'Enter iLO Password: ' ILOPASSWORD
     read -rep 'Enter iLO IP/hostname: ' ILOHOST
-    sed -ri "s/your username/$ILOUSERNAME/" autofan.sh
-    sed -ri "s/your password/$ILOPASSWORD/" autofan.sh
-    sed -ri "s/your ilo ip/$ILOHOST/" autofan.sh
-    mv autofan.sh /usr/bin
+    sed -ri "s/your username/$ILOUSERNAME/" autofan
+    sed -ri "s/your password/$ILOPASSWORD/" autofan
+    sed -ri "s/your ilo ip/$ILOHOST/" autofan
+    rm -f /usr/bin/autofan
+    mv autofan /usr/bin
 
     echo -e "\e[92mDone! Please visit the GitHub page to follow the instructions!\e[0m"
     echo -e "\e[1\https://github.com/That-Guy-Jack/HP-ILO-Fan-Control\e[0m"
